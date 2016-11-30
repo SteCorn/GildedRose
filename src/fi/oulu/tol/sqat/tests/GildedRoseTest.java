cd57a07b41c30e67f43c2eead3202ac1a264a79f
SteCorn <s.cornacchia7@studenti.uniba.it>
2016-11-30 11:32:17 +0100

    metà

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -1,12 +1,31 @@
 package fi.oulu.tol.sqat.tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
-public class GildedRoseTest {
+import fi.oulu.tol.sqat.GildedRose;
+import fi.oulu.tol.sqat.Item;
 
+public class GildedRoseTest {
+	
+	GildedRose gil;
+	ArrayList<Item> ite;
+	
+	
 	@Test
 	public void testTheTruth() {
 		assertTrue(true);
+	}
+	
+	@Test
+	public void testupdate(){
+		
+		gil.main(null);
+		ite = new ArrayList<Item>();
+		ite.add(new Item("ciao",0,30));
+		assertEquals(ite.get(0).getQuality(),18);
 	}
 }
